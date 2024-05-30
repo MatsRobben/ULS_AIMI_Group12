3bec0487c1ab04bc5c535caeed1cf15c126567d5
MatsRobben <77966004+MatsRobben@users.noreply.github.com>
2024-05-30 07:49:06 +0200

    Update baseline_uls_val_job.sh
    Fixed some errors

--- scripts/baseline_uls_val_job.sh
+++ scripts/baseline_uls_val_job.sh
@@ -3,9 +3,9 @@
 #SBATCH --partition=gpu
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=6
+#SBATCH --cpus-per-task=8
 #SBATCH --gpus=1
-#SBATCH --mem=12G
+#SBATCH --mem=16G
 #SBATCH --time=02:00:00
 #SBATCH --gpus-per-node=1
 
@@ -18,18 +18,39 @@ module load Python/3.10.4-GCCcore-11.3.0
 python3 -m pip install --user --upgrade pip
 
 # Clone nnUNet and install all requirements
-cp -r $HOME/uls_baseline/nnUNet .
+git clone --no-checkout https://github.com/MIC-DKFZ/nnUNet.git
 cd nnUNet
+git checkout 947eafbb9adb5eb06b9171330b4688e006e6f301
 pip3 install -e .
 
-# Change these lines to flush the STDOUT, otherwhise it does not update the output file during training.
-sed -i '463s/.*/                print(*args, flush=True)/' nnunetv2/training/nnUNetTrainer/nnUNetTrainer.py
-sed -i '465s/.*/            print(*args, flush=True)/' nnunetv2/training/nnUNetTrainer/nnUNetTrainer.py
+mkdir data data/nnUNet_raw data/nnUNet_results data/nnUNet_preprocessed
 
 # Create env variables
 export nnUNet_raw="$TMPDIR/nnUNet/data/nnUNet_raw"
 export nnUNet_results="$TMPDIR/nnUNet/data/nnUNet_results"
 export nnUNet_preprocessed="$TMPDIR/nnUNet/data/nnUNet_preprocessed"
+
+# Change these lines to flush the STDOUT, otherwhise it does not update the output file during training.
+sed -i '463s/.*/                print(*args, flush=True)/' nnunetv2/training/nnUNetTrainer/nnUNetTrainer.py
+sed -i '465s/.*/            print(*args, flush=True)/' nnunetv2/training/nnUNetTrainer/nnUNetTrainer.py
+
+# copy extentions
+cp -r $HOME/ULS23/baseline_model/architecture/extensions/nnunetv2/ .
+
+# Copy model
+cp $HOME/Dataset001_ULS.zip $nnUNet_results
+unzip $nnUNet_results/Dataset001_ULS.zip
+rm $nnUNet_results/Dataset001_ULS.zip
+cp $nnUNet_results/Dataset001_ULS/nnUNetTrainer_ULS_500_QuarterLR__nnUNetPlansNoRs__3d_fullres_resenc/fold_all/checkpoint_best.pth \
+$nnUNet_results/Dataset001_ULS/nnUNetTrainer_ULS_500_QuarterLR__nnUNetPlansNoRs__3d_fullres_resenc/fold_all/checkpoint_final.pth
+
+# Copy plans file
+mkdir $nnUNet_preprocessed/Dataset001_ULS
+cp $nnUNet_results/Dataset001_ULS/nnUNetTrainer_ULS_500_QuarterLR__nnUNetPlansNoRs__3d_fullres_resenc/plans.json \
+$nnUNet_preprocessed/Dataset001_ULS/nnUNetPlansNoRs.json
+
+# Copy data
+cp -r $HOME/data/Dataset001_ULS/ $nnUNet_raw/Dataset001_ULS
 
 # Process the data and make a plans
 nnUNetv2_extract_fingerprint -d 1

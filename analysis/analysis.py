0edeb534d68f3a434c5f8607b175689c1be1bbd9
mats willem <matsr@LAPTOP-CF87JPEN>
2024-05-31 09:35:53 +0200

    added volume in mm3 and results of baseline

--- analysis/analysis.py
+++ analysis/analysis.py
@@ -14,7 +14,7 @@ true_labels_source_dir = 'data/Dataset001_ULS/labelsTr/'
 predicted_labels_source_dir = 'validation/'
 
 # Define the path to the Parquet file
-parquet_file_path = 'lesion_metrics.parquet'
+parquet_file_path = 'lesion_metrics_baseline.parquet'
 
 # Load the Parquet file into a pandas DataFrame
 df = pd.read_parquet(parquet_file_path)
@@ -29,21 +29,21 @@ print(df)
 columns_to_plot = [col for col in df.columns if col != 'case_name']
 
 # Define the directory to save the images
-save_directory = 'images'
+save_directory = 'images/baseline'
 
 plot_histograms(df, columns_to_plot, save_directory)
-plot_scatterplots(df, [('Dice', 'lesion_size_error')], save_directory)
-plot_scatterplots(df, [('Dice', 'lesion_size_error')], save_directory)
-plot_scatterplots(df, [('Dice', 'true_lesion_size'), ('Dice', 'predicted_lesion_size')], save_directory)
-plot_histograms_two_groups(df, ['true_lesion_size', 'predicted_lesion_size', 'predicted_num_components'], save_directory)
-plot_scatterplots(df, [('predicted_lesion_size', 'true_lesion_size')], save_directory)
-plot_scatterplots(df, [('true_lesion_size', 'predicted_num_components'), ('predicted_lesion_size', 'predicted_num_components')], save_directory)
+plot_scatterplots(df, [('Dice', 'lesion_size_voxels_error')], save_directory)
+plot_scatterplots(df, [('Dice', 'lesion_size_voxels_error')], save_directory)
+plot_scatterplots(df, [('Dice', 'true_lesion_size_voxels'), ('Dice', 'predicted_lesion_size_voxels')], save_directory)
+plot_histograms_two_groups(df, ['true_lesion_size_voxels', 'predicted_lesion_size_voxels', 'predicted_num_components'], save_directory)
+plot_scatterplots(df, [('predicted_lesion_size_voxels', 'true_lesion_size_voxels')], save_directory)
+plot_scatterplots(df, [('true_lesion_size_voxels', 'predicted_num_components'), ('predicted_lesion_size_voxels', 'predicted_num_components')], save_directory)
 
 # Define the directory to save the images
-save_directory = 'images/zero_group'
+save_directory = 'images/baseline/zero_group'
 
 df = df[df['Dice'] == 0]
 
 plot_histograms(df, columns_to_plot, save_directory)
-plot_scatterplots(df, [('predicted_lesion_size', 'true_lesion_size')], save_directory)
-plot_scatterplots(df, [('true_lesion_size', 'predicted_num_components'), ('predicted_lesion_size', 'predicted_num_components')], save_directory)
+plot_scatterplots(df, [('predicted_lesion_size_voxels', 'true_lesion_size_voxels')], save_directory)
+plot_scatterplots(df, [('true_lesion_size_voxels', 'predicted_num_components'), ('predicted_lesion_size_voxels', 'predicted_num_components')], save_directory)
